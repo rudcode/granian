dd44d19cd26652fe30007e754c40ba7952ea4c93
Rudy Nurhadi <rudynurhadi@gmail.com>
2024-11-28 07:02:44 +0700

    test keep reference

--- granian/_futures.py
+++ granian/_futures.py
@@ -1,4 +1,23 @@
+import asyncio
+
+
+class AsyncTaskKeepReference:
+    tasks = set()
+
+    def __init__(self, func):
+        self.async_func = func
+
+    async def __call__(self, *args, **kwargs):
+        task = asyncio.current_task()
+        self.tasks.add(task)
+        try:
+            result = await self.async_func(*args, **kwargs)
+        finally:
+            self.tasks.remove(task)
+        return result
+
 def future_watcher_wrapper(inner):
+    @AsyncTaskKeepReference
     async def future_watcher(watcher):
         try:
             await inner(watcher.scope, watcher.proto)
